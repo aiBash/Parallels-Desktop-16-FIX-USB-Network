dbb5c809102e10d8c9f256d3f45c28b6c433c124
MiKillCrafter <MiKillCrafter@yandex.ru>
2021-08-30 19:52:50 +0300

    Update Parallels_fix_USB_Network.sh

--- Parallels_fix_USB_Network.sh
+++ Parallels_fix_USB_Network.sh
@@ -11,9 +11,8 @@ PARALLELS_DIR="/Library/Preferences/Parallels"  # Путь к папке в ко
 usb_fix() {
 MKC_txt
   MKC_txt " >> USB FIX"
-    grep -rl 'Usb>0' $PARALLELS_DIR/dispatcher.desktop.xml | xargs perl -p -i -e 's/Usb>0/Usb>1/g'
-    sleep 1
-  MKC_txt " << "
+    sudo grep -rl 'Usb>0' $PARALLELS_DIR/dispatcher.desktop.xml | xargs perl -p -i -e 's/Usb>0/Usb>1/g'
+  MKC_txt "$(tput setaf 1) << USB FIX = DONE"
 MKC_txt
 menu
 }
@@ -22,11 +21,10 @@ network_fix() {
 MKC_txt
   MKC_txt " >> Network FIX"
     MKC_txt " >> Try search and replace: UseKextless>-1"
-      grep -rl 'UseKextless>-1' $PARALLELS_DIR/network.desktop.xml | xargs perl -p -i -e 's/UseKextless>-1/UseKextless>0/g'
+      sudo grep -rl 'UseKextless>-1' $PARALLELS_DIR/network.desktop.xml | xargs perl -p -i -e 's/UseKextless>-1/UseKextless>0/g'
     MKC_txt " >> Try search and replace: UseKextless>1"
-      grep -rl 'UseKextless>1' $PARALLELS_DIR/network.desktop.xml | xargs perl -p -i -e 's/UseKextless>1/UseKextless>0/g'
-  sleep 2
-  MKC_txt " << "
+      sudo grep -rl 'UseKextless>1' $PARALLELS_DIR/network.desktop.xml | xargs perl -p -i -e 's/UseKextless>1/UseKextless>0/g'
+  MKC_txt "$(tput setaf 1) << Network FIX = DONE"
 MKC_txt
 menu
 }
@@ -34,37 +32,19 @@ menu
 ################################################################################################
 
 menu() {
-clear
+MKC_txt "$(tput setaf 1)########################################################"
 MKC_txt "Menu:"
-	MKC_txt
-	  MKC_txt " - 1 – FIX USB"
-    MKC_txt " - 2 – FIX Network"
-	MKC_txt
-	  MKC_txt " - 0 – Exit"
-	MKC_txt
-	read -p "...: " case
-	case $case in
-		1) usb_fix;;
-		2) network_fix;;
-		0) exit;;
-  esac
+	MKC_txt " - 1 – FIX USB"
+  MKC_txt " - 2 – FIX Network"
+MKC_txt
+	MKC_txt " - 0 – Exit"
+MKC_txt
+read -p "...: " case
+case $case in
+	1) usb_fix;;
+	2) network_fix;;
+	0) exit;;
+esac
 }
 
-start() {
-  if [ "$(id -u)" -ne 0 ]; then
-    clear; MKC_txt "You are not a root | Вы не root"; else clear
-    MKC_txt "Запустить скрипт для исправления Сети или USB в Parallels Desktop v.16?"
-    MKC_txt "Run the script for fix the Network or USB in Parallels Desktop v.16?"
-    MKC_txt
-      MKC_txt " - 1 - ДА – YES ✔"
-      MKC_txt " - 0 - НЕT – NO ✔"
-    MKC_txt
-		read -p "...: " case
-    case $case in
-      1) menu;;
-      0) exit;;
-    esac;
-  fi
-}
-
-start
+menu
